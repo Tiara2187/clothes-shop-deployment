053c992d7a3ff91e21d09928c4573ee65871e6f3
tiara2187 <tiaramuttianingtyas23@gmail.com>
2021-05-14 23:27:49 +0700

    push button di detail-product

--- src/app/components/product/product-detail/product-detail.component.ts
+++ src/app/components/product/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../Shared-Api/service/cart.service';
 import { ProductService } from '../../Shared-Api/service/product.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class ProductDetailComponent implements OnInit {
   data :any;
   
 
-  constructor(private route : ActivatedRoute, private productService : ProductService) { }
+  constructor(private route : ActivatedRoute, private productService : ProductService, private cartService : CartService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['_id']
@@ -24,6 +25,12 @@ export class ProductDetailComponent implements OnInit {
       this.data = data.product
       console.log(this.data);
       
+    })
+  }
+  addCart(product) {
+    this.cartService.addCart({
+      product,
+      quantity : 1
     })
   }
 
